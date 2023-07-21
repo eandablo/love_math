f32665b417c89178195c32037bd6b668eb154c3a
eandablo <>
2023-07-21 17:25:03 +0000

    display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,20 +6,26 @@ document.addEventListener('DOMContentLoaded',function (){
             alert("You clicked submit");
         } else{
             let gameType=this.getAttribute('data-type');
-            alert(`you clicked ${gameType}`);
+            runGame(gameType);
         }
     });
   }
+  runGame("addition");
 }
 );
 /**
  * The main game 'loop' called when script is first loaded
  * and after the user´s answer has been processed
  */
-function runGame(){
+function runGame(gameType){
   let num1=Math.floor(Math.random()*25+1);
   let num2 = Math.floor(Math.random() * 25 + 1);
-
+  if (gameType==="addition") {
+    displayAdditionQuestion(num1,num2);
+  } else {
+    alert("Type of game unknown");
+    throw 'Unknown game type, Aborting';
+  }
 }
 function aswerCheck(){
 
@@ -33,8 +39,10 @@ function incrementScore(){
 function incrementWrongAnswer(){
 
 }
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1,operand2){
+    document.getElementById('operand-1').textContent=operand1;
+    document.getElementById('operand-2').textContent=operand2;
+    document.getElementById('operator').textContent='+';
 }
 function displaySubstractQuestion() {
 
